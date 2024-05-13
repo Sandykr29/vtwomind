dec5be47364037e3a1d0e824971ee793f8aabbbe
sandykr29 <sandeepkumar29774@yahoo.com>
2024-05-13 10:45:16 +0530

    markup

--- src/component/testfiles/Render.jsx
+++ src/component/testfiles/Render.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 
+
+const Stringcheck=(data,value)=>{
+  if(data.value==="String" || data.value==="Number"){
+    
+  }
+}
+
+const Datamarkup=(item,index,handleClick,handleChange)=>{
+return <>
+<input onChange={(e) => handleChange(e, item)} type="checkbox" name="" id="" />
+<label onClick={() => handleClick(item)} htmlFor="">{item}</label></>
+}
+
 export const Render = ({ newData, handleClick, handleChange }) => {
   return (
     <div className="container">
       {Array.isArray(newData) ? newData.map((item, index) => (
         <div key={index}>
-          <input onChange={(e) => handleChange(e, item)} type="checkbox" name="" id="" />
-          <label onClick={() => handleClick(item)} htmlFor="">{item}</label>
+          {Datamarkup(item,index,handleClick,handleChange)}
         </div>
-      )) : Object.keys(newData).map((item, _) => (
+      )) : Object.keys(newData).map((item, index) => (
         <div key={item}>
-          <input onChange={(e) => handleChange(e, item)} type="checkbox" name="" id="" />
-          <label onChange={(e) => handleChange(e, item)} onClick={() => handleClick(item)} htmlFor="">{item}</label>
+          {Datamarkup(item,index,handleClick,handleChange)}
         </div>
       ))}
     </div>
