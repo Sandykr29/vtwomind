1bed6b5160bf77d51bfd2ef83e8b084e171a2260
sandykr29 <sandeepkumar29774@yahoo.com>
2024-05-13 10:53:44 +0530

    checkbox logic pending

--- src/component/testfiles/Render.jsx
+++ src/component/testfiles/Render.jsx
@@ -3,26 +3,30 @@ import React from 'react';
 
 const Stringcheck=(data,value)=>{
   if(data.value==="String" || data.value==="Number"){
-    
+    {(newData.item=="String" ||newData.item==="Number")?newData.item:item}
   }
 }
 
-const Datamarkup=(item,index,handleClick,handleChange)=>{
-return <>
-<input onChange={(e) => handleChange(e, item)} type="checkbox" name="" id="" />
-<label onClick={() => handleClick(item)} htmlFor="">{item}</label></>
-}
+const Datamarkup = (newData, item, index, handleClick, handleChange) => (
+  <>
+    <input onChange={(e) => handleChange(e, item)} type="checkbox" name="" id="" />
+    <label onClick={() => handleClick(item)}>
+      {(typeof newData[item] === "string" || typeof newData[item] === "number") ? newData[item] : item}
+    </label>
+  </>
+);
+
 
 export const Render = ({ newData, handleClick, handleChange }) => {
   return (
     <div className="container">
       {Array.isArray(newData) ? newData.map((item, index) => (
         <div key={index}>
-          {Datamarkup(item,index,handleClick,handleChange)}
+          {Datamarkup(newData,item,index,handleClick,handleChange)}
         </div>
       )) : Object.keys(newData).map((item, index) => (
         <div key={item}>
-          {Datamarkup(item,index,handleClick,handleChange)}
+          {Datamarkup(newData,item,index,handleClick,handleChange)}
         </div>
       ))}
     </div>
