1d9d0f2ab1e3ac5b60b193d0e9e3a0f5cbcb73cf
sandykr29 <sandeepkumar29774@yahoo.com>
2024-05-13 10:30:58 +0530

    dataupdate

--- src/data.js
+++ src/data.js
@@ -1,25 +1,25 @@
 export const data = {
     music: {
-      hello: "Hello",
-        'see you again': "See you again",
-      arijit: ["Tum Hi Ho", "Kya Chahiye", "Hai Dil Ye Mera", "Kesariya", "Shayad"],
-      atif: ["Adat", "Wo Lamhe", "Tu Jane Na"]
+      hello: "Hello.mp3",
+        'see you again': "See you again.mp3",
+      arijit: ["Tum Hi Ho.mp3", "Kya Chahiye.mp3", "Hai Dil Ye Mera.mp3", "Kesariya.mp3", "Shayad.mp3"],
+      atif: ["Adat", "Wo Lamhe.mp3", "Tu Jane Na.mp3"]
     },
     documents: {
-      "document1": "Document 1",
-      "document2": "Document 2",
-      "document3": "Document 3",
-      "document4": "Document 4",
-      "document5": "Document 5"
+      "document1": "Document 1.pdf",
+      "document2": "Document 2.pdf",
+      "document3": "Document 3.pdf",
+      "document4": "Document 4.pdf",
+      "document5": "Document 5.pdf"
     },
     videos: {
-      "video1": "Video 1",
-      "video2": "Video 2",
-      "video3": "Video 3",
-      "video4": "Video 4",
-      video5: ["Video 5--> in array","video 6--> in array","video 7--> in array"]
+      "video1": "Video 1.mp4",
+      "video2": "Video 2.mp4",
+      "video3": "Video 3.mp4",
+      "video4": "Video 4.mp4",
+      Movies: ["Avatar.mp4","KGF.mp4","RR.mp4"]
     },
-    files:["file A","file B","file C"],
+    files:["file A.pdf","file B.pdf","file C.pdf"],
     objects:{
       data1:"Data one to copy",
       data2:"Data two to copy",
